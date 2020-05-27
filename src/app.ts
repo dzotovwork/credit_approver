0a408a1c55b361f1dd6c996519afb587266e5b60
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-28 02:15:59 +0300

    feat: forms

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-const message: string = 'hello';
-console.log('app.ts');
 const btn: Element = document.querySelector('#btn')!;
 btn.addEventListener('click', () => {
     alert('все работает и это хорошо');
