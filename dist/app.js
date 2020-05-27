bb97a1d2aa84e5640654a7b6461b08d81259135e
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-27 23:55:24 +0300

    fix: add dist

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const message = 'hello';
+console.log('app.ts');
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    alert('все работает и это хорошо');
+});
+//# sourceMappingURL=app.js.map
