73b054def21d53f88fbad003e687980a923bee6f
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-28 09:02:40 +0300

    feat: forms

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const btn = document.getElementById('send');
-const age = document.getElementById('age');
+
 btn.addEventListener('click', () => {
-    console.log(`aloha ${age.value}`);
+    const data = {
+        age: document.getElementById('age').value,
+        sex: document.getElementById('sex').value,
+        money_income: document.getElementById('money_income').value,
+        last_year_money: document.getElementById('last_year_money').value,
+        rating: document.getElementById('rating').value,
+        credit: document.getElementById('credit').value,
+        time: document.getElementById('time').value,
+        goal: document.getElementById('goal').value,
+    };
+    console.log(`data ${JSON.stringify(data)}`);
     alert('все работает и это хорошо');
 });
