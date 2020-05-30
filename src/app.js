f75cbd9d7d20a8c3eefaca9195a04756a0abe36e
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-30 16:43:52 +0300

    feat: forms

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const btn = document.getElementById('send');
 const form = document.getElementById('form');
 form.addEventListener('submit', () => {
-    console.log('form submit');
     const data = {
-        age: document.getElementById('age').value,
-        sex: document.getElementById('sex').value,
-        money_income: document.getElementById('money_income').value,
-        last_year_money: document.getElementById('last_year_money').value,
-        rating: document.getElementById('rating').value,
-        credit: document.getElementById('credit').value,
-        time: document.getElementById('time').value,
-        goal: document.getElementById('goal').value,
+        age: form.age.value,
+        sex: form.sex.value,
+        money_income: form.money_income.value,
+        last_year_money: form.last_year_money.value,
+        rating: form.rating.value,
+        credit: form.credit.value,
+        time: form.time.value,
+        goal: form.goal.value,
     };
     console.log(`data ${JSON.stringify(data)}`);
     alert('все работает и это хорошо');
