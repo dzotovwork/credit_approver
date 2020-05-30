d818ae7e899df93997c08526ac8c6685363c38ff
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-30 18:14:27 +0300

    feat: checkRetirementAge

--- src/test.ts
+++ src/test.ts
@@ -2,13 +2,48 @@ import { checkRetirementAge } from './checkRetirementAge';
 import { checkMoneyTime } from './checkMoneyTime';
 import { dataType } from './dataType';
 
-function checkRaring(data: dataType) {
+/**
+ * Если кредитный рейтинг -2 --> кредит не выдаётся
+ * @param data
+ */
+function checkRating(data: dataType) {
     return data.rating === -2;
+}
+
+/**
+ * Если в источнике дохода указано "безработный" --> кредит не выдаётся
+ * @param data
+ */
+function checkMoneyIncome(data: dataType) {
+    return data.money_income === 'безработный';
+}
+
+/**
+ * метод расчета годового платежа TODO
+ * @param data
+ */
+function getPayment(data: dataType) {
+    return 10;
+}
+
+/**
+ * Если годовой платёж (включая проценты) больше половины дохода --> кредит не выдаётся
+ * @param data
+ */
+function checkPayment(data: dataType) {
+    const yearPayment = getPayment(data); // метод для расчета платежа с процентами
+    return data.last_year_money / 2 < yearPayment;
 }
 
 function checkData(data: dataType) {
     let result = 'Кредит выдаётся';
-    if (!checkRetirementAge(data) || !checkMoneyTime(data) || !checkRaring(data)) {
+    if (
+        !checkRetirementAge(data) ||
+        !checkMoneyTime(data) ||
+        !checkRating(data) ||
+        !checkMoneyIncome(data) ||
+        !checkPayment(data)
+    ) {
         result = 'Кредит не выдаётся';
     }
     return result;
