e49647aaf8e61c0db3b2c1cdd91ad99bea9a3019
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-31 20:44:03 +0300

    works

--- src/calc/__tests__/correction.test.js
+++ src/calc/__tests__/correction.test.js
@@ -15,22 +15,24 @@ describe('Расчет одобренной суммы кредита', () => {
         reporter.epic('Расчет одобренной суммы кредита');
     });
     describe('В зависимости от кредитного рейтинга', () => {
-        const params = [
-            { money_income: 'собственный бизнес', result: 0.2025 },
-            { money_income: 'наёмный работник', result: 0.1975 },
-            { money_income: 'пассивный доход', result: 0.205 },
-        ];
         beforeEach(() => {
             reporter.feature('В зависимости от кредитного рейтинга');
         });
-        params.forEach((param) => {
-            it(`Для заемщика с источником дохода: ${param.money_income}`, async () => {
-                const cloneData = cloneDeep(data);
-                cloneData.money_income = param.money_income;
-                reporter.startStep(`calc ${JSON.stringify(cloneData)}`);
-                reporter.addAttachment('Данные: ', JSON.stringify(cloneData), 'application/json');
-                expect(calc(cloneData)).toBe(param.result);
-                reporter.endStep();
+        describe('Рейтинг: 2', () => {
+            const params = [{ credit: 10.1, result: 1.85 }];
+            const cloneData = cloneDeep(data);
+            cloneData.rating = 2;
+            beforeEach(() => {
+                reporter.story('Рейтинг: 2');
+            });
+            params.forEach((param) => {
+                it(`Для заемщика с рейтингом: 2 и запрашиваемой суммой: ${param.credit} млн`, async () => {
+                    cloneData.credit = param.credit;
+                    reporter.startStep(`calc ${JSON.stringify(cloneData)}`);
+                    reporter.addAttachment('Данные: ', JSON.stringify(cloneData), 'application/json');
+                    expect(calc(cloneData)).toBe(param.result);
+                    reporter.endStep();
+                });
             });
         });
     });
