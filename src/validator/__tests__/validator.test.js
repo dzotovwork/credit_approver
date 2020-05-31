214a5b0721407963f0a6559890a327f33723e589
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-31 15:13:26 +0300

    works

--- src/validator/__tests__/validator.test.js
+++ src/validator/__tests__/validator.test.js
@@ -2,14 +2,26 @@ import checkRetirementAge from './../checkRetirementAge.js';
 describe('Валидация выдачи кредита', () => {
     describe('Если возраст превышает пенсионный возраст на момент возврата кредита --> кредит не выдаётся', () => {
         describe('Мужчины', () => {
-            it('Возраст на момент возврата кредита = 65', async () => {
-                reporter.startStep('возраст 60m , срок 5');
+            it('Возраст на момент возврата кредита = 65, кредит выдается', async () => {
+                reporter.startStep('возраст 60 , срок 5, кредит не выдается');
                 expect(checkRetirementAge('M', 60, 5)).toBe(true);
                 reporter.endStep();
             });
             it('Возраст на момент возврата кредита = 66', async () => {
-                reporter.startStep('возраст 60m , срок 6');
+                reporter.startStep('возраст 60 , срок 6');
                 expect(checkRetirementAge('M', 60, 6)).toBe(false);
+                reporter.endStep();
+            });
+        });
+        describe('Женщины', () => {
+            it('Возраст на момент возврата кредита = 60, кредит выдается', async () => {
+                reporter.startStep('возраст 55 , срок 5');
+                expect(checkRetirementAge('F', 55, 5)).toBe(true);
+                reporter.endStep();
+            });
+            it('Возраст на момент возврата кредита = 61, кредит не выдается', async () => {
+                reporter.startStep('возраст 55 , срок 6');
+                expect(checkRetirementAge('F', 55, 6)).toBe(false);
                 reporter.endStep();
             });
         });
