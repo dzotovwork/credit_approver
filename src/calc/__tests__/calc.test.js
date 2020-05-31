b4f9c1334672537dd7eda11135cd713430e3c6be
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-31 17:29:00 +0300

    works

--- src/calc/__tests__/calc.test.js
+++ src/calc/__tests__/calc.test.js
@@ -5,23 +5,31 @@ describe('Калькуляция кредита', () => {
         reporter.epic('Калькуляция кредита');
     });
     describe('В зависимости от источника дохода', () => {
+        const data = {
+            age: 20,
+            sex: 'F',
+            money_income: '',
+            last_year_money: 10,
+            rating: 0,
+            credit: 1,
+            time: 10,
+            goal: 'автокредит',
+        };
+        const params = [
+            { money_income: 'собственный бизнес', result: 0.2025 },
+            { money_income: 'наёмный работник', result: 0.1975 },
+            { money_income: 'пассивный доход', result: 0.205 },
+        ];
         beforeEach(() => {
             reporter.feature('В зависимости от источника дохода');
         });
-        it('Для заемщиков с собственным бизнесом', async () => {
-            const data = {
-                age: 20,
-                sex: 'F',
-                money_income: 'собственный бизнес',
-                last_year_money: 10,
-                rating: 0,
-                credit: 1,
-                time: 10,
-                goal: 'автокредит',
-            };
-            reporter.startStep(`calc ${JSON.stringify(data)}`);
-            expect(calc(data)).toBe(0.2025);
-            reporter.endStep();
+        params.forEach((param) => {
+            it(`Для заемщика с источником дохода: ${param.money_income}`, async () => {
+                data.money_income = param.money_income;
+                reporter.startStep(`calc ${JSON.stringify(data)}`);
+                expect(calc(data)).toBe(param.result);
+                reporter.endStep();
+            });
         });
     });
 });
