9e5a9f81a1616af11af2f34bf1a29bc50ad5cc43
Зотов Дмитрий Вадимович <d.zotov@tinkoff.ru>
2020-05-31 15:17:55 +0300

    works

--- src/validator/__tests__/validator.test.js
+++ src/validator/__tests__/validator.test.js
@@ -1,29 +1,37 @@
 import checkRetirementAge from './../checkRetirementAge.js';
 describe('Валидация выдачи кредита', () => {
-    describe('Если возраст превышает пенсионный возраст на момент возврата кредита --> кредит не выдаётся', () => {
-        describe('Мужчины', () => {
-            it('Возраст на момент возврата кредита = 65, кредит выдается', async () => {
-                reporter.startStep('возраст 60 , срок 5, кредит не выдается');
-                expect(checkRetirementAge('M', 60, 5)).toBe(true);
-                reporter.endStep();
-            });
-            it('Возраст на момент возврата кредита = 66', async () => {
-                reporter.startStep('возраст 60 , срок 6');
-                expect(checkRetirementAge('M', 60, 6)).toBe(false);
-                reporter.endStep();
-            });
+    beforeEach(() => {
+        reporter.epic('Валидация выдачи кредита');
+        reporter.feature('Если возраст превышает пенсионный возраст на момент возврата кредита --> кредит не выдаётся');
+    });
+    describe('Мужчины', () => {
+        beforeEach(() => {
+            reporter.story('Мужчины');
         });
-        describe('Женщины', () => {
-            it('Возраст на момент возврата кредита = 60, кредит выдается', async () => {
-                reporter.startStep('возраст 55 , срок 5');
-                expect(checkRetirementAge('F', 55, 5)).toBe(true);
-                reporter.endStep();
-            });
-            it('Возраст на момент возврата кредита = 61, кредит не выдается', async () => {
-                reporter.startStep('возраст 55 , срок 6');
-                expect(checkRetirementAge('F', 55, 6)).toBe(false);
-                reporter.endStep();
-            });
+        it('Возраст на момент возврата кредита = 65, кредит выдается', async () => {
+            reporter.startStep('возраст 60 , срок 5, кредит не выдается');
+            expect(checkRetirementAge('M', 60, 5)).toBe(true);
+            reporter.endStep();
+        });
+        it('Возраст на момент возврата кредита = 66', async () => {
+            reporter.startStep('возраст 60 , срок 6');
+            expect(checkRetirementAge('M', 60, 6)).toBe(false);
+            reporter.endStep();
+        });
+    });
+    describe('Женщины', () => {
+        beforeEach(() => {
+            reporter.story('Женщины');
+        });
+        it('Возраст на момент возврата кредита = 60, кредит выдается', async () => {
+            reporter.startStep('возраст 55 , срок 5');
+            expect(checkRetirementAge('F', 55, 5)).toBe(true);
+            reporter.endStep();
+        });
+        it('Возраст на момент возврата кредита = 61, кредит не выдается', async () => {
+            reporter.startStep('возраст 55 , срок 6');
+            expect(checkRetirementAge('F', 55, 6)).toBe(false);
+            reporter.endStep();
         });
     });
 });
